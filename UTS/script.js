316fcfe270c1f1e9fb00c8a431c2e10c188328f2
soviaviay <soviaviay04@gmail.com>
2022-04-12 09:34:00 -0700

    menambah warna

--- UTS/script.js
+++ UTS/script.js
@@ -5,3 +5,11 @@ document.addEventListener("mousemove", e => {
     Kursor.setAttribute("style", "top: " + e.pageY + "px; left: " + e.pageX + "px;")
 });
 
+function random(Mycolor) {
+    return Math.floor(Math.random() * Mycolor);
+}
+
+document.onclick  = function () { 
+Warnawarni = "rgb(" + random(255) + "," + random(255) + "," + random(255) + ")";
+Kursor.style.background = Warnawarni;
+}
